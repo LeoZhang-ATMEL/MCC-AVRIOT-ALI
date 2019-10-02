6092dfd00c411cc623a5ce0877f564a2d2ffcc1d
Leo Zhang <103140506@qq.com>
2019-10-03 00:05:56 +0800

    AVR-IOT AC164160 EVB Alibaba Demo

---
+++ mcc_generated_files/config/cloud_config.h
@@ -0,0 +1,37 @@
+#ifndef CLOUD_CONFIG_H
+#define CLOUD_CONFIG_H
+
+// <h> Cloud Configuration
+
+// <s> project id
+// <i> Google Cloud Platform project id
+// <id> project_id
+#define CFG_PRODUCT_KEY "a11v7xKKAWn"
+
+
+// <s> project region
+// <i> Google Cloud Platform project region
+// <id> project_region
+#define CFG_DEVICE_NAME "AC5C"
+
+#define CFG_DEVICE_SECRET "uzbhqsD8gRFuwX4HmUJiMShMmxclNMVu"
+
+#define CFG_WRITE_DEVICE_SECRET 1
+
+#define MAX_PRODUCT_KEY_LENGTH 11
+#define MAX_DEVICE_NAME_LENGTH 30
+#define DEVICE_SECRET_LENGTH 32
+
+// <s> registry id
+// <i> Google Cloud Platform registry id
+// <id> registry_id
+#define CFG_MQTT_HOST_SUFFIX ".iot-as-mqtt.cn-shanghai.aliyuncs.com"
+
+#define CFG_MQTT_HOST CFG_PRODUCT_KEY CFG_MQTT_HOST_SUFFIX
+
+
+#define MQTT_COMM_PORT_TLS 1883
+
+// </h>
+
+#endif // CLOUD_CONFIG_H
